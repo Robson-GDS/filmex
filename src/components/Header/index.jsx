907e9d6e266f94d82b6a2aefa06ebb846b197ccd
unknown <robson.080490@gmail.com>
2022-07-04 14:49:57 -0300

    estruturando header do app

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,18 @@
+import { NavLink } from 'react-router-dom';
+
+import { Container, Content, Favoritos, Logo } from './style';
+
 export function Header() {
   return (
-    <h1>Header</h1>
+    <Container>
+      <Content>
+        <Logo>
+          <NavLink to="/">Filmex</NavLink>
+        </Logo>
+        <Favoritos>
+          <NavLink to="/favorito">Meus filmes</NavLink>
+        </Favoritos>
+      </Content>
+    </Container>
   )
 }
