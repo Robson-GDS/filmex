9bbaeded33cac7e68a72b85f83bf7b438810a3c6
unknown <robson.080490@gmail.com>
2022-07-04 15:03:56 -0300

    estilizando header

---
+++ src/components/Header/style.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: var(--black-theme);
+  height: 6rem;
+`
+
+export const Content = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  height: 6rem;
+  padding: 0 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    text-decoration: none;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
+`
+
+export const Logo = styled.h1`
+  font-size: 3rem;
+
+  a {
+    color: var(--white-text);
+  }
+`
+
+export const Favoritos = styled.a`
+
+`
