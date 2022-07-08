96be65fb11f0ff7743bcf77ddb4f8326ae8753b3
unknown <robson.080490@gmail.com>
2022-07-08 11:24:44 -0300

    consumindo dados da api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react"
+
+import api from "../../services/api"
+
 export function Home() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+
+    async function loadMovies() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "215676f43605ace1c8ac8237af04ecf1",
+          language: "pt-BR",
+          page: 1,
+        }
+      })
+    }
+
+    loadMovies();
+  }, [])
+
   return (
     <h1>Pagina Home</h1>
   )
