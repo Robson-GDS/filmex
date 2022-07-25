ce1382bd0cba7c4807b431ddd9123ac29711952c
unknown <robson.080490@gmail.com>
2022-07-25 20:06:05 -0300

    recebendo api na pagina home

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -1,8 +1,23 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import api from "../../services/api";
+
 export function Movie() {
   const { id } = useParams();
+
+  useEffect(() => {
+    async function loadMovie() {
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: "215676f43605ace1c8ac8237af04ecf1",
+          language: "pt-BR",
+        }
+      })
+    }
+
+    loadMovie();
+  }, [])
 
   return (
     <h1>Acessando filme {id}</h1>
