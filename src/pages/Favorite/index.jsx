10c1df5ad31527166b784d05a154069e0350eb1c
unknown <robson.080490@gmail.com>
2022-08-17 06:56:04 -0300

    estlizando pagina favorite

--- src/pages/Favorite/index.jsx
+++ src/pages/Favorite/index.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
+import { Container, Content } from "./style";
+
 export function Favorite() {
   const [movies, setMovies] = useState([]);
 
---
+++ src/pages/Favorite/style.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 2rem;
+`;
+
+export const Content = styled.div`
+  h1 {
+    padding: 24px;
+    text-align: center;
+  }
+
+  h3 {
+    text-align: center;
+  }
+
+  span {
+    font-size: 18px;
+
+    @media (max-width: 750px) {
+      margin-bottom: 10px;
+    }
+  }
+
+  li {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 24px;
+
+    @media (max-width: 750px) {
+      flex-direction: column;
+    }
+  }
+  
+  div {
+
+a {
+  text-decoration: none;
+  margin-right: 10px;
+  background-color: var(--btn-detalhes-filmes);
+  color: var(--white-text);
+  padding: 10px 20px;
+  border-radius: 5px;
+
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
+
+}
+
+button {
+  padding: 11px 20px;
+  border-radius: 5px;
+  background-color: var(--btn-excluir);
+  color: var(--white-text);
+  text-transform: uppercase;
+
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
+}
+`
