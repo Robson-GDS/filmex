a6a9777e5abd15adf2e8991c12ab1078577efa7d
unknown <robson.080490@gmail.com>
2022-08-17 06:34:21 -0300

    estruturando pagina favoritos

--- src/pages/Favorite/index.jsx
+++ src/pages/Favorite/index.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+
 export function Favorite() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const myList = localStorage.getItem("@filmex")
+    setMovies(JSON.parse(myList) || [])
+  }, [])
+
   return (
-    <h1>Pagina favoritos</h1>
+    <Container>
+      <Content>
+        <h1>Meus Filmes</h1>
+
+        {movies.length === 0 && <h3>Ops, não temos filmes salvos no momento!</h3>}
+
+        <ul>
+          {movies.map((item) => {
+            return (
+              <li key={item.id}>
+                <span>{item.title}</span>
+                <div>
+                  <Link to={`/movie/${item.id}`}>Ver detalhes</Link>
+                  <button >Excluir</button>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </Content>
+    </Container>
   )
 }
